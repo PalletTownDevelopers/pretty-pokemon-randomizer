14f5f6d438891f7940ab7adb86e25230111c06eb
guillaumefabrizi <guillaume.fabrizi@outlook.com>
2019-09-09 23:52:28 +0200

    replace viridian to vermilion, shuffle arenas wip

--- src/main/java/com/likeageek/randomizer/App.java
+++ src/main/java/com/likeageek/randomizer/App.java
@@ -1,13 +1,50 @@
 package com.likeageek.randomizer;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.shuffle;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    protected int seed;
+    protected Map<String, String> shuffleArenas = new HashMap<>();
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public String convertAsmFile() throws URISyntaxException, IOException {
+        String viridianCityAsm = new String(Files.readAllBytes(Paths.get(getClass().getResource("ViridianCity.asm").toURI())));
+
+        String[] lines = viridianCityAsm.split("\n\t");
+        String viridianGymElement = replace(lines[7]);
+        lines[7] = viridianGymElement;
+
+        return String.join("\n\t", lines);
+    }
+
+    public void shuffleArenas() {
+        List<String> arenas = asList("Viridian", "Vermilion");
+
+        List<String> shuffledArenas = new ArrayList<>();
+        shuffledArenas.addAll(arenas);
+        shuffle(shuffledArenas, new Random(seed));
+
+        for (int i = 0; i < arenas.size(); i++) {
+            shuffleArenas.put(arenas.get(i), shuffledArenas.get(i));
+        }
+    }
+
+    private String replace(String line) {
+        String[] viridianGymLineElement = line.split(",");
+        viridianGymLineElement[3] = " VERMILION_GYM" + "\n";
+        return String.join(",", viridianGymLineElement);
     }
 }
--- src/test/java/com/likeageek/randomizer/AppTest.java
+++ src/test/java/com/likeageek/randomizer/AppTest.java
@@ -1,20 +1,31 @@
 package com.likeageek.randomizer;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+import java.io.*;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.*;
+
+
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+    App app = new App();
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldReplaceViridianGymToVermilionGym() throws URISyntaxException, IOException {
+        String outputFileContent = app.convertAsmFile();
+
+        String viridianCityExpected = new String(Files.readAllBytes(Paths.get(getClass().getResource("ViridianCity.txt").toURI())));
+        assertEquals(viridianCityExpected, outputFileContent);
+    }
+
+    @Test
+    public void shouldShuffleArenas(){
+        app.seed = 3297392;
+        app.shuffleArenas();
+        assertEquals("Vermilion", app.shuffleArenas.get("Viridian"));
     }
 }
