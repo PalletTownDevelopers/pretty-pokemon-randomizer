3407834546020ad2e7e805d3a200e78d3ef88751
Yoann Fievez <yoann.fievez@gmail.com>
2019-10-06 13:46:10 +0200

    Add run.sh to launch jar more easier

---
+++ run.sh
@@ -0,0 +1,1 @@
+java -jar target/randomizer-1.0-SNAPSHOT.jar -shake -seed 4242424242 -pokemon_dir "/home/likeageek/Projects/randomizer-cache/" -output_dir "/home/likeageek/Projects/randomizer-output/"
