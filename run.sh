d9922cd498b48001e347bdd5ab087914b47d144e
Fievez Yoann <yoann.fievez@gmail.com>
2019-10-28 20:05:22 +0100

    Sync java files

--- run.sh
+++ run.sh
@@ -1,1 +1,23 @@
-java -jar target/randomizer-1.0-SNAPSHOT-jar-with-dependencies.jar -shake -seed 4242424242 -pokemon_dir "/home/likeageek/Projects/randomizer-cache/" -output_dir "/home/likeageek/Projects/randomizer-output/"
+DEV_MODE=1
+
+if [[ $DEV_MODE == 1 ]]; then
+  OUTPUT_DIR="$HOME/wd/pokemon/randomizer-output/"
+  CACHE_DIR="$HOME/wd/pokemon/randomizer-cache/"
+fi
+
+#Launch shaker
+java -jar target/randomizer-1.0-SNAPSHOT-jar-with-dependencies.jar \
+-shake \
+-seed 4242424242 \
+-pokemon_dir "$CACHE_DIR" \
+-output_dir "$OUTPUT_DIR"
+
+#Go to output directory and delete old gbc roms
+cd "$OUTPUT_DIR"
+rm ./*.gbc
+
+#We add permissions to use tools to help compilation
+chmod -Rf 777 ./*
+
+#We compile ASM code to build GBC roms
+make
