e23c73628835efd1007e901886d4e95f8781adfa
FIEVEZ Yoann <yoann.fievez@gmail.com>
2020-06-07 21:42:01 +0200

    feat(global):Refactorize gitignor and update ui

--- ui/app.js
+++ ui/app.js
@@ -1,32 +1,46 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const path = require('path');
-const router = express.Router();
-const exec = require('child_process').exec;
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const path = require('path')
+const router = express.Router()
+const exec = require('child_process').exec
 
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname+'/public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
-router.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname+'/public/index.html'));
-});
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname+'/public/index.html'))
+})
 
-router.post('/generate', function (req, res) {
-    var params = req.body;
-    exec('../run.sh', (error, stdout, stderr) => {
-        if (error) {
-            console.error(`exec error: ${error}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
-    });
-    res.sendFile(path.join(__dirname+'/public/index.html'));
-});
+router.post('/generate', (req, res) => {
+    let params = req.body
+    
+    let seed = params.seed
+    let randomizerOutput = __dirname + '/tmp/randomizer-output'
+    let randomizerCache = __dirname + '/tmp/randomizer-cache'
+    let codeDisassembly = __dirname + '/tmp/pokered/'
 
-app.use('/',router);
-app.use('/generate',router);
-app.listen(3000, function () {
+    let commandRandomizer = 'sh run.sh ' + randomizerOutput + ' ' + randomizerCache + ' ' + codeDisassembly + ' ' + seed
+    let processRandomizer = exec(commandRandomizer)
+    console.log(commandRandomizer)
+
+    processRandomizer.stdout.on('data', (data) => {
+        console.log('stdout: ' + data.toString())
+    })
+
+    processRandomizer.stderr.on('data', (data) => {
+        console.log('stderr: ' + data.toString())
+    })
+
+    processRandomizer.on('exit', (code) => {
+        console.log('child process exited with code ' + code.toString())
+    })
+
+    res.sendFile(path.join(__dirname+'/public/index.html'))
+})
+
+app.use('/',router)
+app.use('/generate',router)
+app.listen(3000, () => {
     console.log('Example app listening on port 3000!')
 })
