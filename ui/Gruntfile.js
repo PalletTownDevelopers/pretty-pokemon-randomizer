7481e506c9f1efc8c3f6d1dd02026806551e9429
Fievez Yoann <yoann.fievez@gmail.com>
2020-10-23 19:16:31 +0200

    Update makefile for ui and add icon of fontawesome

--- ui/Gruntfile.js
+++ ui/Gruntfile.js
@@ -1,14 +1,14 @@
 module.exports = function(grunt) {
-    
+
     const sass = require('node-sass');
-     
+
     require('load-grunt-tasks')(grunt);
-     
+
     grunt.initConfig({
         sass: {
             options: {
                 implementation: sass,
-                sourceMap: true
+                sourceMap: false
             },
             dist: {
                 files: {
